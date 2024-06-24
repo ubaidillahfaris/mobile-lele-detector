9fce088aa10771706d8403f36e8786bca55b0abd
faris <ubaidillah.faris99@gmail.com>
2024-06-25 00:46:24 +0700

    fix: setting time video record

--- lib/page/camera.dart
+++ lib/page/camera.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:image_picker/image_picker.dart';
@@ -16,34 +19,124 @@ class VideoStreamPage extends StatefulWidget {
 
 class _VideoStreamPageState extends State<VideoStreamPage> {
   File? video_path;
-  RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   VideoPlayerController? videoPlayerController;
   MediaStream? _localStream;
   bool isFrontCamera = true;
   bool isStreaming = false;
+  bool? isLoading = false;
+  CameraController? _cameraController;
+  String? _videoPath;
   Timer? _streamingTimer;
-  String? _temporaryVideoPath;
-  File? _temporaryVideoFile;
-  IOSink? _videoFileSink;
-  bool?  isLoading = false;
+  XFile? videoFile;
+
 
   @override
   void initState() {
     super.initState();
-    initRenderers();
-    startVideoStream();
   }
 
   @override
   void dispose() {
-    _localRenderer.dispose();
-    _localStream?.dispose();
-    _streamingTimer?.cancel();
-    _videoFileSink?.close();
     super.dispose();
+    if(_streamingTimer != null){
+      _streamingTimer?.cancel();
+    }
+    if (isStreaming == true) {
+      stopVideoRecording();
+    }
+    if (_cameraController != null) {
+      _cameraController?.dispose();
+    }
+  }
+  
+  void streamingSetting() async {
+    
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+
+    _cameraController = CameraController(firstCamera, ResolutionPreset.high);
+
+    await _cameraController!.initialize();
+
+    final appDir = await getApplicationDocumentsDirectory();
+    _videoPath = '${appDir.path}/video.mp4';
+    setState(() {
+      isStreaming = true;
+    });
+    _streamingTimer = Timer.periodic(Duration(seconds: 10), (timer) {
+      onVideoRecordButtonPressed();
+      Timer(Duration(seconds: 10), () {
+        onStopButtonPressed();
+      });
+    });
+
   }
 
+    void onVideoRecordButtonPressed() {
+      startVideoRecording().then((_) {
+        if (mounted) {
+         setState(() {
+            
+          });
+        }
+      });
+    }
 
+
+    Future<void> startVideoRecording() async {
+
+      final CameraController? cameraController = _cameraController;
+
+      if (cameraController == null || !cameraController.value.isInitialized) {
+        print('Error: select a camera first.');
+        return;
+      }
+
+      if (cameraController.value.isRecordingVideo) {
+        // A recording is already started, do nothing.
+        return;
+      }
+
+      try {
+        await cameraController.startVideoRecording();
+      } on CameraException catch (e) {
+        print(e);
+        return;
+      }
+    }
+
+    
+    void onStopButtonPressed() {
+      stopVideoRecording().then((XFile? file) {
+        if (mounted) {
+          setState(() {});
+        }
+        if (file != null) {
+          print('Video recorded to ${file.path}');
+          videoFile = file;
+          sendVideo(file.path);
+          // _startVideoPlayer();
+        }
+      });
+    }
+
+   Future<XFile?> stopVideoRecording() async {
+
+    final CameraController? cameraController = _cameraController;
+
+    if (cameraController == null || !cameraController.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      return cameraController.stopVideoRecording();
+    } on CameraException catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // upload video from local storage
   Future<void> _pickVideo() async {
     final pickedFile = await ImagePicker().pickVideo(source: ImageSource.gallery);
     if (pickedFile != null) {
@@ -57,43 +150,8 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
       });
     }
   }
-
-  void uploadVideo(){
-    if (video_path != null) {
-      sendVideo(video_path!.path, true);
-    }
-  }
-
-  Future<void> initRenderers() async {
-    await _localRenderer.initialize();
-  }
-
-  Future<void> startVideoStream() async {
-    final Map<String, dynamic> mediaConstraints = {
-      'audio': false,
-      'video': {
-        'mandatory': {
-          'minWidth': '640',
-          'minHeight': '480',
-          'minFrameRate': '30',
-        },
-        'facingMode': isFrontCamera ? 'user' : 'environment',
-        'optional': [],
-      }
-    };
-
-    try {
-      MediaStream stream =
-          await navigator.mediaDevices.getUserMedia(mediaConstraints);
-      _localRenderer.srcObject = stream;
-      _localStream = stream;
-
-      setState(() {});
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
+  
+  // switch camera
   Future<void> switchCamera() async {
     if (_localStream == null) return;
     final track = _localStream!.getVideoTracks().first;
@@ -102,92 +160,73 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
     setState(() {});
   }
 
-  Future<void> startStreaming() async {
-    if (_localStream == null) return;
-    print('Start Stream');
-
-    final directory = await getApplicationDocumentsDirectory();
-    _temporaryVideoPath = '${directory.path}/temporary_video.mp4';
-
-    _temporaryVideoFile = File(_temporaryVideoPath!);
-    _videoFileSink = _temporaryVideoFile!.openWrite();
-
-    setState(() {
-      isStreaming = true;
-    });
-
-    _streamingTimer = Timer.periodic(Duration(milliseconds: 1000), (timer) async {
-      if (!isStreaming) {
-        timer.cancel();
-        return;
-      }
-
-      final track = _localStream!.getVideoTracks().first;
-      var frame = await track.captureFrame();
-      await saveFrameToFile(frame);
-    });
-
-    Timer(Duration(seconds: 20), () {
-      stopStreaming();
-    });
-  }
-
-  Future<void> saveFrameToFile(ByteBuffer frameBuffer) async {
-    Uint8List frame = frameBuffer.asUint8List();
-    _videoFileSink!.add(frame);
-  }
-
-  Future<void> stopStreaming() async {
-    if (!isStreaming) return;
-    
-    setState(() {
-      isStreaming = false;
-    });
-    _streamingTimer?.cancel();
-    await _videoFileSink?.close();
-    await sendVideo(_temporaryVideoPath!);
-  }
-
+  // upload video to api
   Future<void> sendVideo(String filePath, [bool fromStorage = false]) async {
+  if (fromStorage == false) {
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('${ConfigApp.baseUrl}/process_video'),
+    );
 
-    if (fromStorage == false) {
-      
-      var request = http.MultipartRequest(
-        'POST',
-        Uri.parse('${ConfigApp.baseUrl}/process_video'),
-      );
-      request.files.add(
-        await http.MultipartFile.fromPath('file', filePath),
-      );
+    final file = File(filePath);
+    final length = await file.length();
 
-      var response = await request.send();
+    if (length == 0) {
+      print('Error: File size is zero bytes');
+      return;
+    } else {
+      print('File size: $length bytes');
+    }
 
-      if (response.statusCode == 200) {
-        print('Video berhasil dikirim');
-        // Hapus file video sementara jika pengiriman berhasil
-        File temporaryFile = File(filePath);
-        await temporaryFile.delete();
-        startStreaming();
-      } else {
-        print('Gagal mengirim video: ${response.statusCode}');
-      }
-    }else{
-      setState(() {
-        isLoading = true;
-      });
-      var request = http.MultipartRequest(
-        'POST',
-        Uri.parse('${ConfigApp.baseUrl}/process_video'),
-      );
-      request.files.add(
-        await http.MultipartFile.fromPath('file', filePath),
-      );
-      
-      var response = await request.send().then((result) {
-        Navigator.pop(context);
-      });
+    request.files.add(
+      await http.MultipartFile.fromPath('file', filePath),
+    );
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      print('Video sent successfully');
+    } else {
+      print('Failed to send video: ${response.statusCode}');
+    }
+
+    File temporaryFile = File(filePath);
+    await temporaryFile.delete();
+
+  } else {
+    setState(() {
+      isLoading = true;
+    });
+
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('${ConfigApp.baseUrl}/process_video'),
+    );
+
+    final file = File(filePath);
+    final length = await file.length();
+
+    if (length == 0) {
+      print('Error: File size is zero bytes');
+      return;
+    } else {
+      print('File size: $length bytes');
+    }
+
+    request.files.add(
+      await http.MultipartFile.fromPath('file', filePath),
+    );
+
+    var response = await request.send().then((result) {
+      Navigator.pop(context);
+    });
+
+    if (response.statusCode == 200) {
+      print('Video sent successfully');
+    } else {
+      print('Failed to send video: ${response.statusCode}');
     }
   }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -206,50 +245,47 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
       body: Column(
         children: [
           Expanded(
-            child: Stack(children: [
-              if(video_path == null) ...{
-                RTCVideoView(_localRenderer),
-                if(isStreaming == true)...{
+            child: Stack(
+              children: [
+                if (video_path != null) ...{
                     Container(
-                    width: width,
-                    padding: EdgeInsets.only(right: 10,top: 20),
-                    child: Text('recording',
-                      textAlign: TextAlign.end,
-                      style: TextStyle( 
-                        color: Colors.red.shade500,
-                        fontSize: 18
+                      width: width,
+                      child: AspectRatio(
+                        aspectRatio: videoPlayerController!.value.aspectRatio,
+                        child: VideoPlayer(videoPlayerController!),
                       ),
-                    ),
-                  )
-                }
-              },
-              if(video_path != null) ...{
-                Container( 
-                  width: width,
-                  child:  AspectRatio(
-                    aspectRatio: videoPlayerController!.value.aspectRatio,
-                    child: VideoPlayer(videoPlayerController!),
-                  ),
-                )
-              }
-            ]),
+                    )
+                  },
+                  if (isStreaming == true && _cameraController != null) ...{
+                    Container(
+                      width: width,
+                      height: height * 0.5,
+                      child: AspectRatio(
+                        aspectRatio: _cameraController!.value.aspectRatio,
+                        child: CameraPreview(_cameraController!),
+                      ),
+                    )
+                  }
+                ],
+                
+                
+            ),
           ),
-          if(isLoading == false)...{
-
+          if (isLoading == false) ...{
             Container(
               height: height * 0.25,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  if(isStreaming == false)...{
+                  if (isStreaming == false) ...{
                     FilledButton(
-                      style: ButtonStyle( 
-                        elevation: WidgetStatePropertyAll(0),
-                        backgroundColor: WidgetStatePropertyAll( 
-                          Colors.grey.shade400
-                        )
-                      ),
-                      onPressed: startStreaming,
+                      style: ButtonStyle(
+                          elevation: WidgetStatePropertyAll(0),
+                          backgroundColor: WidgetStatePropertyAll(
+                              Colors.grey.shade400)),
+                      onPressed: (){
+                       streamingSetting();
+                      },
                       child: Icon(
                         Icons.fiber_manual_record_sharp,
                         size: 56,
@@ -257,15 +293,15 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
                       ),
                     ),
                   },
-                  if(isStreaming == true)...{
+                  if (isStreaming == true) ...{
                     FilledButton(
-                      style: ButtonStyle( 
-                        elevation: WidgetStatePropertyAll(0),
-                        backgroundColor: WidgetStatePropertyAll( 
-                          Colors.grey.shade400
-                        )
-                      ),
-                      onPressed: stopStreaming,
+                      style: ButtonStyle(
+                          elevation: WidgetStatePropertyAll(0),
+                          backgroundColor: WidgetStatePropertyAll(
+                              Colors.grey.shade400)),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                       child: Icon(
                         Icons.stop,
                         size: 56,
@@ -273,55 +309,53 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
                       ),
                     ),
                   },
-                  SizedBox(height: height * 0.03,),
-                  Column( 
-                    children: [ 
+                  SizedBox(
+                    height: height * 0.03,
+                  ),
+                  Column(
+                    children: [
                       Text(
                         'atau unggah video',
-                        style: TextStyle( 
-                          color: Colors.grey.shade500
-                        ),
+                        style: TextStyle(color: Colors.grey.shade500),
                       ),
-                      SizedBox(height: height * 0.01,),
-                      if(video_path == null) ...{
+                      SizedBox(
+                        height: height * 0.01,
+                      ),
+                      if (video_path == null) ...{
                         ElevatedButton.icon(
-                            style: ButtonStyle( 
+                          style: ButtonStyle(
                               elevation: WidgetStatePropertyAll(0),
                               backgroundColor: WidgetStatePropertyAll(
-                                ConfigApp.colors['primary']
-                              )
-                            ),
-                            icon: Icon(
-                              Icons.phone_iphone_sharp,
-                              color: Colors.white,
-                            ),
-                            onPressed: _pickVideo,
-                            label: Text(
-                              'Pilih video dari galeri',
-                              style: TextStyle( 
-                                color: Colors.white
-                              ),
-                            ),
+                                  ConfigApp.colors['primary'])),
+                          icon: Icon(
+                            Icons.phone_iphone_sharp,
+                            color: Colors.white,
+                          ),
+                          onPressed: _pickVideo,
+                          label: Text(
+                            'Pilih video dari galeri',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       },
-                      if(video_path != null) ...{
+                      if (video_path != null) ...{
                         ElevatedButton.icon(
-                          style: ButtonStyle( 
+                          style: ButtonStyle(
                               elevation: WidgetStatePropertyAll(0),
                               backgroundColor: WidgetStatePropertyAll(
-                                ConfigApp.colors['primary']
-                              )
-                            ),
-                            icon: Icon(
-                              Icons.file_upload_outlined,
-                              color: Colors.white,
+                                  ConfigApp.colors['primary'])),
+                          icon: Icon(
+                            Icons.file_upload_outlined,
+                            color: Colors.white,
                           ),
-                          onPressed: uploadVideo,
+                          onPressed: (){
+                            if (video_path != null) {
+                              sendVideo(video_path?.path??'',true);
+                            }
+                          },
                           label: Text(
                             'Upload Video',
-                            style: TextStyle( 
-                              color: Colors.white
-                            ),
+                            style: TextStyle(color: Colors.white),
                           ),
                         )
                       },
@@ -331,12 +365,11 @@ class _VideoStreamPageState extends State<VideoStreamPage> {
               ),
             ),
           },
-          if(isLoading == true) ...{
+          if (isLoading == true) ...{
             Container(
               height: height * 0.25,
               child: Center(child: CircularProgressIndicator()),
             )
-            
           }
         ],
       ),
